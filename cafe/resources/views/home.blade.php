1d9fe3e2964ba46d75bef9f7c5736b53bd0266da
Irina Zhyzhyian <gygyjan.iryna@gmail.com>
2020-12-06 20:54:04 +0200

    Added styles for home

--- cafe/resources/views/home.blade.php
+++ cafe/resources/views/home.blade.php
@@ -4,26 +4,32 @@
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link type="text/css" href="{{ asset('css/styleMenu.css') }}" rel="stylesheet">
 @section('content')
-<a href='/home'><div class="homeuserheader">Особистий кабінет</div></a>
+<div class="wall">
+<a href='/home'><div class="homeuserheader"><img class="img" src="/img/cup.png" alt="cup" >ОСОБИСТИЙ КАБІНЕТ</div></a>
 <div class="container">
     <div class="row content-center">   
         @foreach ($coffee as $row)
         <div class="row coffeecard">
-            <a class="col" href="/customcoffee-edit/{{ $row->id }}">
-                <div class='circle'>
+            <a class="col">
+                <div class='round'>
+                <img class="circle" src="/img/circle.jpg" alt="circle" >
+                    <div class='middle'>
                     <h5 class='coffeeName'>{{$row->name}}</h5>
-                    <h6 class='coffeePrice'>??? грн</h6>
-                    <p>{{$row->composition}}</p>
+                    @foreach(preg_split('/","/',$row->composition) as $l)
+                        <h6 class='coffeePrice'>{{$l}}</h6>
+                    @endforeach
                     <form action="/customcoffee-delete/{{ $row->id }}" mathod="post">
                         {{ csrf_field() }}
                         {{ method_field('GET') }}
                         <button type="submit" class="btn btn-outline-danger">ВИДАЛИТИ</button>
                     </form>
                 </div>
+                </div>
             </a>
         </div> 
         @endforeach        
     </div>
+</div>
 </div>
 </div>
 @endsection
